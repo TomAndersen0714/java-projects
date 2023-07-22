a5a8aee284e9ee62866f8e830cf1c9553617d4dd
TomAndersen <1040994898@qq.com>
2023-07-22 09:38:24 +0800

    update

--- my-second-java-project/java-coding/src/main/java/algorithm/practice/leetcode/LeetCode344.java
+++ my-second-java-project/java-coding/src/main/java/algorithm/practice/leetcode/LeetCode344.java
@@ -11,7 +11,15 @@ public class LeetCode344 {
 
 class LeetCode344_1 {
     public void reverseString(char[] s) {
+        // exclude boundary condition
+        if (s == null || s.length <= 1) {
+            return;
+        }
 
+        // handle
+        int left = 0, right = s.length - 1;
+
+        // return
     }
 
     public static void main(String[] args) {
