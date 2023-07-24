809b8edf299c87599f2c521e0b0298ff751b7f13
TomAndersen <1040994898@qq.com>
2023-07-25 00:19:58 +0800

    update

--- my-second-java-project/java-coding/src/main/java/algorithm/practice/leetcode/LeetCode5.java
+++ my-second-java-project/java-coding/src/main/java/algorithm/practice/leetcode/LeetCode5.java
@@ -12,7 +12,7 @@ public class LeetCode5 {
 }
 
 /**
- * Brute Force: expand from centers
+ * Brute Force: expand from centers to avoid duplicate compute
  * TC:O(n*n), SC:O(1)
  */
 class LeetCode5_1 {
@@ -75,7 +75,58 @@ class LeetCode5_1 {
  * TC:O(n*n), SC:O(n)
  */
 class LeetCode5_2 {
-    public static void main(String[] args) {
+    public String longestPalindrome(String s) {
+        // exclude boundary condition
+        if (s == null || s.length() <= 1) {
+            return s;
+        }
 
+        // handle
+        // transform original problem, and construct state transfer equation
+        // dp[i][j] = true, if(s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1])
+        int length = s.length();
+        boolean[][] dp = new boolean[length][length];
+        int left = 0, right = 0;
+
+        // set the boundary condition for equation
+        // interval = 0
+        for (int i = 0; i < length; i++) {
+            dp[i][i] = true;
+        }
+
+        // iterate from simple problem to the more complex
+        // interval = 1
+        for (int i = 0; i + 1 < length; i++) {
+            int j = i + 1;
+            if (s.charAt(i) == s.charAt(j)) {
+                dp[i][j] = true;
+                left = i;
+                right = j;
+            }
+        }
+
+        // interval = [2, n)
+        for (int interval = 2; interval < length; interval += 1) {
+            for (int i = 0; i + interval < length; i += 1) {
+                int j = i + interval;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    left = i;
+                    right = j;
+                }
+            }
+        }
+
+        // return
+        return s.substring(left, right + 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(
+            new LeetCode5_2().longestPalindrome("babad").equals("bab")
+            || new LeetCode5_2().longestPalindrome("babad").equals("aba")
+        );
+        System.out.println(new LeetCode5_2().longestPalindrome("cbbd").equals("bb"));
+        System.out.println(new LeetCode5_2().longestPalindrome("caba").equals("aba"));
     }
 }
