e9974531bb1bf5e5cc00d6eee428e75d579c53bc
TomAndersen <1040994898@qq.com>
2023-07-29 12:07:55 +0800

    update

--- my-second-java-project/java-coding/src/main/java/algorithm/practice/leetcode/LeetCode59.java
+++ my-second-java-project/java-coding/src/main/java/algorithm/practice/leetcode/LeetCode59.java
@@ -1,7 +1,17 @@
 package algorithm.practice.leetcode;
 
 /**
+ * 59. Spiral Matrix II: https://leetcode.com/problems/spiral-matrix-ii/
+ * <p>
+ * tags: medium
+ *
  * @author TomAndersen
  */
 public class LeetCode59 {
+}
+
+class LeetCode59_1 {
+    public int[][] generateMatrix(int n) {
+
+    }
 }
