d25a05ec8a7785d5a5a70569a1b7bc43c51e98d6
TomAndersen <1040994898@qq.com>
2023-08-28 00:42:28 +0800

    update

--- my-second-java-project/java-coding/src/main/java/algorithm/practice/leetcode/LeetCode200.java
+++ my-second-java-project/java-coding/src/main/java/algorithm/practice/leetcode/LeetCode200.java
@@ -17,50 +17,22 @@ public class LeetCode200 {
 class LeetCode200_1 {
     public int numIslands(char[][] grid) {
         // exclude boundary situation
-        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+        if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
             return 0;
         }
 
-        // dfs recursion
+        // breadth first search
         int counter = 0;
-        boolean[][] isVisited = new boolean[grid.length][grid[0].length];
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1' && !isVisited[i][j]) {
-                    counter += 1;
-                    dfsRecursionVisit(grid, isVisited, i, j);
-                }
-            }
-        }
+        int m = grid.length, n = grid[0].length;
+        boolean[][] isVisited = new boolean[m][n];
+
 
         // return
         return counter;
     }
 
-    private void dfsRecursionVisit(char[][] grid, boolean[][] isVisited, int i, int j) {
-        if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length
-            && !isVisited[i][j] && grid[i][j] == '1') {
-            isVisited[i][j] = true;
-
-            dfsRecursionVisit(grid, isVisited, i + 1, j);
-            dfsRecursionVisit(grid, isVisited, i, j + 1);
-            dfsRecursionVisit(grid, isVisited, i - 1, j);
-            dfsRecursionVisit(grid, isVisited, i, j - 1);
-        }
-    }
 
     public static void main(String[] args) {
-        System.out.println(new LeetCode200_1().numIslands(new char[][]{
-            {'1', '1', '1', '1', '0'},
-            {'1', '1', '0', '1', '0'},
-            {'1', '1', '0', '0', '0'},
-            {'0', '0', '0', '0', '0'}
-        }) == 1);
-        System.out.println(new LeetCode200_1().numIslands(new char[][]{
-            {'1', '1', '0', '0', '0'},
-            {'1', '1', '0', '0', '0'},
-            {'0', '0', '1', '0', '0'},
-            {'0', '0', '0', '1', '1'}
-        }) == 3);
+
     }
 }
