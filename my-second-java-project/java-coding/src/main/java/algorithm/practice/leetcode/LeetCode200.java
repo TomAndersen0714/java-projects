d56eaa1473aa4fcbc6e5dc26cd19e4a48f2856b1
TomAndersen <1040994898@qq.com>
2023-08-28 09:39:40 +0800

    update

--- my-second-java-project/java-coding/src/main/java/algorithm/practice/leetcode/LeetCode200.java
+++ my-second-java-project/java-coding/src/main/java/algorithm/practice/leetcode/LeetCode200.java
@@ -21,16 +21,79 @@ class LeetCode200_1 {
             return 0;
         }
 
-        // breadth first search
+        // iteration and visit all points
         int counter = 0;
         int m = grid.length, n = grid[0].length;
         boolean[][] isVisited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // depth first search by recursion, i.e. expanding from the boundary of island
+                if (grid[i][j] == '1' && !isVisited[i][j]) {
+                    counter += 1;
+                    dfsRecursionVisit(grid, i, j, isVisited);
+                }
+            }
+
+        }
+        // return
+        return counter;
+    }
+
+    private void dfsRecursionVisit(char[][] grid, int x, int y, boolean[][] isVisited) {
+        // exclude boundary situation
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) {
+            return;
+        }
+        if (grid[x][y] != '1' || isVisited[x][y]) {
+            return;
+        }
+
+        // visit specific point
+        isVisited[x][y] = true;
+        // visit surrounding points
+        dfsRecursionVisit(grid, x - 1, y, isVisited);
+        dfsRecursionVisit(grid, x + 1, y, isVisited);
+        dfsRecursionVisit(grid, x, y - 1, isVisited);
+        dfsRecursionVisit(grid, x, y + 1, isVisited);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(
+            new LeetCode200_1().numIslands(new char[][]{
+                {'1', '1', '1', '1', '0'},
+                {'1', '1', '0', '1', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '0', '0', '0'}
+            }) == 1
+        );
+        System.out.println(
+            new LeetCode200_1().numIslands(new char[][]{
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+            }) == 3
+        );
+    }
+}
+
+
+/**
+ * BFS
+ * TC: O(n), SC: O(n)
+ */
+class LeetCode200_2 {
+    public int numIslands(char[][] grid) {
+        // exclude boundary situations
+
+        // breadth first search
+        int counter = 0;
 
 
         // return
         return counter;
     }
-
 
     public static void main(String[] args) {
 
