330323bcdfd311f4730131e48cf0f385ee18ead7
TomAndersen <1040994898@qq.com>
2023-08-29 11:42:15 +0800

    update

---
+++ my-second-java-project/java-coding/src/main/java/algorithm/practice/leetcode/LeetCode207.java
@@ -0,0 +1,27 @@
+package algorithm.practice.leetcode;
+
+/**
+ * 207. Course Schedule: https://leetcode.com/problems/course-schedule/
+ * <p>
+ * tags: medium, graph, bfs, dfs
+ *
+ * @author TomAndersen
+ */
+public class LeetCode207 {
+}
+
+/**
+ *
+ */
+class LeetCode207_1 {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        // exclude boundary situation
+
+
+        // handle
+
+
+        // return
+
+    }
+}
