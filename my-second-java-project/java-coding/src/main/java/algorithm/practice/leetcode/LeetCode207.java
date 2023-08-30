6aa67b140a3963aceae8290f469e29fa62726eb6
TomAndersen <1040994898@qq.com>
2023-08-30 09:47:57 +0800

    update

--- my-second-java-project/java-coding/src/main/java/algorithm/practice/leetcode/LeetCode207.java
+++ my-second-java-project/java-coding/src/main/java/algorithm/practice/leetcode/LeetCode207.java
@@ -1,6 +1,9 @@
 package algorithm.practice.leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * 207. Course Schedule: https://leetcode.com/problems/course-schedule/
@@ -27,17 +30,18 @@ class LeetCode207_1 {
         boolean isPossible = true;
 
         // build graph in adjacency list, and visit all edges in the graph by DFS recursion
-        int[] adjacencyList = new int[numCourses];
+        HashMap<Integer, List<Integer>> outEdges = new HashMap<>();
         // record the state of pointers, 0-not visited, 1-visiting, 2-visited
         int[] states = new int[numCourses];
 
-        Arrays.fill(adjacencyList, -1);
-        for (int[] edge : prerequisites) {
-            adjacencyList[edge[0]] = edge[1];
+        for (int[] outEdge : prerequisites) {
+            List<Integer> outEdgeList = outEdges.getOrDefault(outEdge[0], new ArrayList<>());
+            outEdgeList.add(outEdge[1]);
+            outEdges.put(outEdge[0], outEdgeList);
         }
 
         for (int[] edge : prerequisites) {
-            if (!dfsRecursion(adjacencyList, states, edge[0])) {
+            if (!dfsRecursion(outEdges, states, edge[0])) {
                 isPossible = false;
                 break;
             }
@@ -60,12 +64,22 @@ class LeetCode207_1 {
             return true;
         }
         else {
-            return dfsRecursion(adjacencyList, states, adjacencyList[x]);
+            // access current point
+            states[x] = 1;
+            if (dfsRecursion(adjacencyList, states, adjacencyList[x])) {
+                states[x] = 2;
+                return true;
+            }
+            else {
+                return false;
+            }
         }
     }
 
 
     public static void main(String[] args) {
-
+        System.out.println(new LeetCode207_1().canFinish(2, new int[][]{{1, 0}}) == true);
+        System.out.println(new LeetCode207_1().canFinish(2, new int[][]{{1, 0}, {0, 1}}) == false);
+        System.out.println(new LeetCode207_1().canFinish(3, new int[][]{{1, 0}, {1, 2}, {0, 1}}) == false);
     }
 }
