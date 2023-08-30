7b42e2d2c5c7aa81803024385b451fb981dcb8a8
TomAndersen <1040994898@qq.com>
2023-08-31 00:32:08 +0800

    update

--- my-second-java-project/java-coding/src/main/java/algorithm/practice/leetcode/LeetCode207.java
+++ my-second-java-project/java-coding/src/main/java/algorithm/practice/leetcode/LeetCode207.java
@@ -14,7 +14,7 @@ public class LeetCode207 {
 
 /**
  * DFS recursion
- * TC: O(n), SC: O(n)
+ * TC: O(m+n), SC: O(m+n)
  */
 class LeetCode207_1 {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
@@ -55,7 +55,7 @@ class LeetCode207_1 {
             return true;
         }
         else if (states[x] == 1) {
-            return true;
+            return false;
         }
         else {
             // visit current point and all subsequent points by dfs recursion
@@ -79,4 +79,135 @@ class LeetCode207_1 {
         System.out.println(new LeetCode207_1().canFinish(2, new int[][]{{1, 0}, {0, 1}}) == false);
         System.out.println(new LeetCode207_1().canFinish(3, new int[][]{{1, 0}, {1, 2}, {0, 1}}) == false);
     }
+}
+
+/**
+ * Topological Sort: adjacency list(map.list) + queue
+ * TC: O(m+n), SC: O(m+n)
+ */
+class LeetCode207_2 {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        // exclude boundary situation
+        if (numCourses == 0 || prerequisites == null || prerequisites.length == 0) {
+            return true;
+        }
+
+        // build a graph using adjacency list
+        boolean res = true;
+        int[] inDegrees = new int[numCourses];
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
+
+        for (int[] edge : prerequisites) {
+            inDegrees[edge[0]] += 1;
+            List<Integer> outPoints = adjList.getOrDefault(edge[1], new ArrayList<>());
+            outPoints.add(edge[0]);
+            adjList.put(edge[1], outPoints);
+        }
+
+        // try to iterate all points by topological level order
+        // initialize queue
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < inDegrees.length; i++) {
+            if (inDegrees[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int visitCounter = 0;
+        while (!queue.isEmpty()) {
+            Integer headPoint = queue.poll();
+            visitCounter += 1;
+
+            // iterate all subsequent points of current point
+            for (int nextPoint : adjList.getOrDefault(headPoint, Collections.emptyList())) {
+                inDegrees[nextPoint] -= 1;
+                if (inDegrees[nextPoint] == 0) {
+                    queue.offer(nextPoint);
+                }
+            }
+        }
+
+        if (visitCounter != numCourses) {
+            res = false;
+        }
+
+        // return
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LeetCode207_2().canFinish(2, new int[][]{{1, 0}}) == true);
+        System.out.println(new LeetCode207_2().canFinish(2, new int[][]{{1, 0}, {0, 1}}) == false);
+        System.out.println(new LeetCode207_2().canFinish(3, new int[][]{{1, 0}, {1, 2}, {0, 1}}) == false);
+    }
+}
+
+
+/**
+ * Topological Sort(BFS): adjacency list(list.list) + queue
+ * TC: O(m+n), SC: O(m+n)
+ */
+class LeetCode207_3 {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        // exclude boundary situation
+        if (numCourses == 0 || prerequisites == null || prerequisites.length == 0) {
+            return true;
+        }
+
+        // build graph and iterate by topological order
+        boolean res = true;
+        List<List<Integer>> adjList = new ArrayList<>();
+        int[] inDegrees = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            adjList.add(new ArrayList<>());
+        }
+        for (int[] edge : prerequisites) {
+            inDegrees[edge[0]] += 1;
+            adjList.get(edge[1]).add(edge[0]);
+        }
+
+        // try to iterate all points by topological level order
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < inDegrees.length; i++) {
+            if (inDegrees[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        int visitCounter = 0;
+        while (!queue.isEmpty()) {
+            int head = queue.poll();
+            visitCounter += 1;
+
+            for (int next : adjList.get(head)) {
+                inDegrees[next] -= 1;
+                if (inDegrees[next] == 0) {
+                    queue.offer(next);
+                }
+            }
+        }
+
+        if (visitCounter != numCourses) {
+            res = false;
+        }
+
+        // return result
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LeetCode207_3().canFinish(2, new int[][]{{1, 0}}) == true);
+        System.out.println(new LeetCode207_3().canFinish(2, new int[][]{{1, 0}, {0, 1}}) == false);
+        System.out.println(new LeetCode207_3().canFinish(3, new int[][]{{1, 0}, {1, 2}, {0, 1}}) == false);
+    }
+}
+
+/**
+ * DFS recursion
+ * TC: O(m+n), SC: O(m+n)
+ */
+class LeetCode207_4 {
+
+
 }
