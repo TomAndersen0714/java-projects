b66c36cb59c37b0e642418f05202e0cbd12b29e3
TomAndersen <1040994898@qq.com>
2023-09-14 09:04:20 +0800

    update

---
+++ my-second-java-project/java-coding/src/main/java/algorithm/practice/leetcode/LeetCode399.java
@@ -0,0 +1,22 @@
+package algorithm.practice.leetcode;
+
+import java.util.List;
+
+/**
+ * 399. Evaluate Division: https://leetcode.com/problems/evaluate-division/
+ *
+ * @author TomAndersen
+ */
+public class LeetCode399 {
+}
+
+class LeetCode399_1 {
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        // exclude boundary situations
+
+
+        // solve problem
+
+        // return
+    }
+}
