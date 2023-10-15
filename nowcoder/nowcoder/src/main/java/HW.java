5197490acfae74f798ac2c41603b819c6b3e62db
TomAndersen <1040994898@qq.com>
2023-10-15 16:21:05 +0800

    update

--- nowcoder/nowcoder/src/main/java/HW.java
+++ nowcoder/nowcoder/src/main/java/HW.java
@@ -1,37 +1,48 @@
-import java.util.PriorityQueue;
-
 /**
  * @author TomAndersen
  */
 public class HW {
-    int calculate(int[] nums) {
+    public int landCount(int[][] matrix) {
         // exclude boundary situations
-        if (nums == null || nums.length == 0) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return 0;
         }
 
         // handle
-        int n = nums.length, res;
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1, o2) -> (o2 - o1));
-        for (int num : nums) {
-            maxHeap.offer(num);
-        }
+        int res = 0;
+        int m = matrix.length, n = matrix[0].length;
+        boolean[][] isVisited = new boolean[m][n];
 
-        while (maxHeap.size() >= 2) {
-            int x = maxHeap.poll(), y = maxHeap.poll();
-            if (x != y) {
-                maxHeap.offer(x > y ? x - y : y - x);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1 && !isVisited[i][j]) {
+                    res += 1;
+                    dfsMark(matrix, m, n, isVisited, i, j);
+                }
             }
         }
 
         // return
-        res = maxHeap.size() == 1 ? maxHeap.poll() : 0;
         return res;
     }
 
+    private void dfsMark(int[][] matrix, int m, int n, boolean[][] isVisited, int x, int y) {
+        // exclude boundary situations
+        if (x > m - 1 || x < 0 || y > n - 1 || y < 0 || matrix[x][y] != 1 || isVisited[x][y]) {
+            return;
+        }
+
+        // mark
+        isVisited[x][y] = true;
+
+        // dfs recursion
+        dfsMark(matrix, m, n, isVisited, x - 1, y);
+        dfsMark(matrix, m, n, isVisited, x + 1, y);
+        dfsMark(matrix, m, n, isVisited, x, y - 1);
+        dfsMark(matrix, m, n, isVisited, x, y + 1);
+    }
+
     public static void main(String[] args) {
-        System.out.println(new HW().calculate(new int[]{2, 7, 4, 1, 8, 1}));
-        System.out.println(new HW().calculate(new int[]{7, 8}));
-        System.out.println(new HW().calculate(new int[]{3, 2, 1}));
+
     }
 }
