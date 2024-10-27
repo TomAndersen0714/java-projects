8ffc820118b7cfc1583086a041822595bc0515c2
Tom Andersen <1040994898@qq.com>
2024-10-28 01:49:11 +0800

    feat: java-problems 增加常用util

--- java-project-second/java-problems/src/main/java/leetcode/common/Util.java
+++ java-project-second/java-problems/src/main/java/leetcode/common/Util.java
@@ -18,50 +18,54 @@ public class Util {
         return array;
     }
 
+    // todo: 修复二维List的比较失败问题
     public static <E> boolean collEquals(Collection<E> x, Collection<E> y) {
         List<E> collectedListX = x.stream().sorted().collect(Collectors.toList());
         List<E> collectedListY = y.stream().sorted().collect(Collectors.toList());
         return collectedListX.equals(collectedListY);
     }
 
-    public static List<?> arrayToList(Object array) {
-        if (array == null) {
+    public static List<Object> arrayDeepToList(Object array) {
+        if (array == null || !array.getClass().isArray()) {
             return null;
         }
 
-        // 获取数组的组件类型
+        // 获取数组的元素类型
         Class<?> componentType = array.getClass().getComponentType();
 
-        // 如果组件类型是数组，则递归调用
+        int length = Array.getLength(array);
+        List<Object> list = new ArrayList<>(length);
+        // 如果元素类型是数组，则递归调用
         if (componentType.isArray()) {
-            int length = Array.getLength(array);
-            List<Object> list = new ArrayList<>(length);
             for (int i = 0; i < length; i++) {
-                list.add(arrayToList(Array.get(array, i)));
+                list.add(arrayDeepToList(Array.get(array, i)));
             }
-            return list;
         }
         else {
             // 否则，将数组转换为List
-            int length = Array.getLength(array);
-            List<Object> list = new ArrayList<>(length);
             for (int i = 0; i < length; i++) {
                 list.add(Array.get(array, i));
             }
-            return list;
         }
+        return list;
     }
 
     public static void main(String[] args) {
         int[] randomIntArray = randomIntArray(4);
         System.out.println(Arrays.toString(randomIntArray));
 
-        System.out.println("equals(List.of(1,2,3), List.of(3,1,2)) = "
+        System.out.println("collEquals(List.of(1,2,3), List.of(3,1,2)) = "
             + collEquals(List.of(1, 2, 3), List.of(3, 1, 2))
         );
 
-        System.out.println("arrayToList(new int[][]{{1, 2, 3}, {3, 4, 5}}) = "
-            + arrayToList(new int[][]{{1, 2, 3}, {3, 4, 5}})
+        collEquals(arrayDeepToList(new int[][]{{1, 2, 3}, {3, 1, 2}}), arrayDeepToList(new int[][]{{1, 2, 3}, {3, 1, 2}}));
+
+        System.out.println("arrayDeepToList(new int[][]{{1, 2, 3}, {3, 4, 5}}) = "
+            + arrayDeepToList(new int[][]{{1, 2, 3}, {3, 4, 5}})
+        );
+
+        System.out.println("arrayDeepToList(new int[]{1, 2, 3}) = "
+            + arrayDeepToList(new int[]{1, 2, 3})
         );
 
         System.out.println("Arrays.equals(new int[]{1, 2, 3}, new int[]{3, 2, 1}) = "
