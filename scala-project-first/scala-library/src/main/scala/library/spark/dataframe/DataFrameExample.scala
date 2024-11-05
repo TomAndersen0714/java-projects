5c11631fe1811a37382e1f04fd4dccdb936a0649
Tom Andersen <1040994898@qq.com>
2024-11-05 15:39:50 +0800

    feat: spark scala api 更新

--- scala-project-first/scala-library/src/main/scala/library/spark/dataframe/DataFrameExample.scala
+++ scala-project-first/scala-library/src/main/scala/library/spark/dataframe/DataFrameExample.scala
@@ -20,13 +20,14 @@ object DataFrameExample {
         // 导入 spark 内置的隐式转换工具, 使用隐式类和隐式转换函数, 进而给 Seq 类新增方法, 即 toDF
         import spark.implicits._
         val df = List(
-          (1, 1, 1, 1, 3),
-          (2, 2, 3, 4, 4)
+          (1, 1, 1, 1, "3"),
+          (2, 2, 3, 4, "4")
         ).toDF("colA", "colB", "colC", "colD", "colE")
 
         // org.apache.spark.sql.Dataset.select(scala.collection.Seq<org.apache.spark.sql.Column>)
         print("org.apache.spark.sql.Column.alias")
         df.select(col("colA").alias("a").alias("a1")).show()
+        df.selectExpr("if(colE = '', 1, 2)").show()
 
         // org.apache.spark.sql.RelationalGroupedDataset.count
         print("org.apache.spark.sql.RelationalGroupedDataset.count")
