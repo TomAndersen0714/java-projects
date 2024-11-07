7c3e704fca58f0cfe5126feb916a8cd1d7711c8a
Tom Andersen <1040994898@qq.com>
2024-11-07 19:38:47 +0800

    feat: java 算法刷题-双指针

---
+++ java-project-second/java-problems/src/main/java/leetcode/second/easy/LeetCode125.java
@@ -0,0 +1,59 @@
+package leetcode.second.easy;
+
+/**
+ * @author TomAndersen
+ */
+public class LeetCode125 {
+    public static void main(String[] args) {
+        String input;
+        boolean expect;
+
+        input = "A man, a plan, a canal: Panama";
+        expect = true;
+        System.out.println(new LeetCode125_1().isPalindrome(input) == expect);
+
+        input = "race a car";
+        expect = false;
+        System.out.println(new LeetCode125_1().isPalindrome(input) == expect);
+
+        input = " ";
+        expect = true;
+        System.out.println(new LeetCode125_1().isPalindrome(input) == expect);
+    }
+}
+
+class LeetCode125_1 {
+    public boolean isPalindrome(String s) {
+        // input
+        if (s == null) {
+            return false;
+        }
+
+        // transform
+        boolean res = true;
+        int left = 0, right = s.length() - 1;
+        char leftChar, rightChar;
+        while (left < right) {
+            leftChar = s.charAt(left);
+            rightChar = s.charAt(right);
+
+            if (!(Character.isAlphabetic(leftChar) || Character.isDigit(leftChar))) {
+                left += 1;
+            }
+            else if (!(Character.isAlphabetic(rightChar) || Character.isDigit(rightChar))) {
+                right -= 1;
+            }
+            else if (Character.toLowerCase(leftChar) == Character.toLowerCase(rightChar)) {
+                left += 1;
+                right -= 1;
+            }
+            else {
+                res = false;
+                break;
+            }
+        }
+
+        // output
+        return res;
+    }
+}
---
+++ java-project-second/java-problems/src/main/java/leetcode/second/hard/LeetCode42.java
@@ -0,0 +1,60 @@
+package leetcode.second.hard;
+
+import leetcode.common.Util;
+
+/**
+ * @author TomAndersen
+ */
+public class LeetCode42 {
+    public static void main(String[] args) {
+        int[] input;
+        int expect;
+
+        input = Util.strToArray("[0,1,0,2,1,0,1,3,2,1,2,1]");
+        expect = 6;
+        System.out.println(new LeetCode42_1().trap(input) == expect);
+
+        input = Util.strToArray("[4,2,0,3,2,5]");
+        expect = 9;
+        System.out.println(new LeetCode42_1().trap(input) == expect);
+    }
+}
+
+/**
+ * Iterate triple times
+ */
+class LeetCode42_1 {
+    public int trap(int[] height) {
+        // input
+        if (height == null || height.length <= 2) {
+            return 0;
+        }
+
+        // transform
+        int sum = 0, trap, length = height.length;
+        int leftCurMax = 0, rightCurMax = 0;
+
+        int[] leftMax = new int[height.length];
+        int[] rightMax = new int[height.length];
+
+        for (int i = 0; i < length; i++) {
+            leftCurMax = Math.max(leftCurMax, height[i]);
+            leftMax[i] = leftCurMax;
+
+            int j = length - i - 1;
+            rightCurMax = Math.max(rightCurMax, height[j]);
+            rightMax[j] = rightCurMax;
+        }
+
+        // calculate
+        for (int i = 0; i < length; i++) {
+            trap = Math.min(leftMax[i], rightMax[i]);
+            if (trap > height[i]) {
+                sum += (trap - height[i]);
+            }
+        }
+
+        // output
+        return sum;
+    }
+}
