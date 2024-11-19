aa4a5b4b85a336e2d24e5684f9e37bf5127ce901
TomAndersen <1040994898@qq.com>
2024-11-19 09:19:07 +0800

    feat: java 算法刷题-动态规划DP

--- java-project-second/java-problems/src/main/java/leetcode/second/hard/LeetCode188.java
+++ java-project-second/java-problems/src/main/java/leetcode/second/hard/LeetCode188.java
@@ -39,7 +39,7 @@ class LeetCode188_1 {
         // transform
         int maxProfit = 0;
 
-        // state array, yes[i][j] 代表第i天进行第j笔交易的买入操作后持有股票时的最大利润, no[i][j] 代表第i天已经进行过第j笔交易的卖出后未持有股票的最大利润, 注意: 同一笔交易的卖出在买入之后
+        // state array, yes[i][j] 代表第i天进行第j笔交易的买入操作后持有股票时的最大利润, no[i][j] 代表第i天已经进行过第j笔交易的卖出后未持有股票的最大利润, 注意: 同一笔交易的卖出在买入之后, 因此前者依赖后者, 后者要先计算
         // state transform equation, yes[i][j] = max{yes[i-1][j], no[i-1][j-1] - price}, no[i][j] = max{no[i-1][j],yes[i-1][j]+price}
 
         int[][] yes = new int[prices.length][k + 1];
@@ -48,6 +48,7 @@ class LeetCode188_1 {
         // initialize dp array, 第0天的所有买入, 利润都为 -prices[0]; 第0次交易，即无交易，利润默认为0
         Arrays.fill(yes[0], 1, k + 1, -prices[0]);
 
+        // iterate and fill dp array
         for (int i = 1; i < prices.length; i += 1) {
             int price = prices[i];
 
